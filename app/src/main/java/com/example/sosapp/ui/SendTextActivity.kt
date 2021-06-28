82adf81e585f09c493e3ac3d5c4ecc27fabe885d
ZSabakh <zsabakhtarishvili@atu.edu>
2021-06-28 16:07:17 +0400

    Can send user and admin template texts

--- app/src/main/java/com/example/sosapp/ui/SendTextActivity.kt
+++ app/src/main/java/com/example/sosapp/ui/SendTextActivity.kt
@@ -3,24 +3,22 @@ package com.example.sosapp.ui
 import android.content.Intent
 import android.location.Location
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.sosapp.ContactsRecyclerViewAdapter
 import com.example.sosapp.LocationHelper
 import com.example.sosapp.R
 import com.example.sosapp.TextsRecyclerViewAdapter
 import com.example.sosapp.api.ApiClient
 import com.example.sosapp.api.SessionManager
 import com.example.sosapp.models.*
-import com.example.sosapp.ui.models.ContactUIModel
 import com.example.sosapp.ui.models.TextUIModel
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.io.Serializable
 
 class SendTextActivity : AppCompatActivity() {
     private lateinit var apiClient: ApiClient
@@ -28,7 +26,7 @@ class SendTextActivity : AppCompatActivity() {
     private lateinit var btSendText: Button
     private lateinit var recyclerView: RecyclerView
     private lateinit var fetchedTexts: MutableList<TextResponse>
-
+    private lateinit var selectedContacts: Serializable
     val coordinates: MutableList<String> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -49,37 +47,10 @@ class SendTextActivity : AppCompatActivity() {
         })
 
 
-        val selectedContacts = intent.getSerializableExtra("selected_contacts")
+        selectedContacts = intent.getSerializableExtra("selected_contacts")!!
 
         btSendText.setOnClickListener {
-            val intent = Intent(this@SendTextActivity, MainActivity::class.java)
-
-            apiClient.getApiService().sendText(
-                token = "${sessionManager.fetchAuthToken()}",
-                MessageRequest(selectedContacts as ArrayList<String>, "TEST TEST", coordinates)
-            )
-                .enqueue(object : Callback<MessageResponse> {
-                    override fun onFailure(call: Call<MessageResponse>, t: Throwable) {
-                        startActivity(intent)
-                    }
-
-                    override fun onResponse(
-                        call: Call<MessageResponse>,
-                        response: Response<MessageResponse>
-                    ) {
-                        if (response.code() != 200) {
-                            Toast.makeText(this@SendTextActivity, "Error", Toast.LENGTH_SHORT)
-                                .show()
-                            startActivity(intent)
-                        }
-                        Toast.makeText(
-                            this@SendTextActivity,
-                            "Text submitted!",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                        startActivity(intent)
-                    }
-                })
+            sendText("test");
         }
     }
 
@@ -105,11 +76,11 @@ class SendTextActivity : AppCompatActivity() {
                         val intent = Intent(this@SendTextActivity, LoginActivity::class.java)
                         startActivity(intent)
                     }
-                    val loginResponse = response.body()
-                    Toast.makeText(this@SendTextActivity, "Contacts fetched!", Toast.LENGTH_SHORT)
+                    val textsResponse = response.body()
+                    Toast.makeText(this@SendTextActivity, "Texts fetched!", Toast.LENGTH_SHORT)
                         .show()
-                    if (loginResponse != null) {
-                        fetchedTexts = loginResponse.texts
+                    if (textsResponse != null) {
+                        fetchedTexts = textsResponse.texts
                         recyclerView.adapter = TextsRecyclerViewAdapter(fetchedTexts.map {
                             println(it)
                             TextUIModel(
@@ -117,7 +88,7 @@ class SendTextActivity : AppCompatActivity() {
                                 it.user_id,
                                 it.text,
                                 onClick = {
-
+                                    sendText(it.text)
                                 },
                             )
                         })
@@ -126,6 +97,36 @@ class SendTextActivity : AppCompatActivity() {
             })
     }
 
+    private fun sendText(text: String){
+        val intent = Intent(this@SendTextActivity, MainActivity::class.java)
+
+        apiClient.getApiService().sendText(
+            token = "${sessionManager.fetchAuthToken()}",
+            MessageRequest(selectedContacts as ArrayList<String>, text, coordinates)
+        )
+            .enqueue(object : Callback<MessageResponse> {
+                override fun onFailure(call: Call<MessageResponse>, t: Throwable) {
+                    startActivity(intent)
+                }
+
+                override fun onResponse(
+                    call: Call<MessageResponse>,
+                    response: Response<MessageResponse>
+                ) {
+                    if (response.code() != 200) {
+                        Toast.makeText(this@SendTextActivity, "Error", Toast.LENGTH_SHORT)
+                            .show()
+                        startActivity(intent)
+                    }
+                    Toast.makeText(
+                        this@SendTextActivity,
+                        "Text submitted!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    startActivity(intent)
+                }
+            })
+    }
     private fun viewInitializations() {
         recyclerView = findViewById(R.id.rv_texts)
         btSendText = findViewById(R.id.bt_select_contacts)
