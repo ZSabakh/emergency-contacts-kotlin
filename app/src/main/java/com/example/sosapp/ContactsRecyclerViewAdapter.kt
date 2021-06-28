c3a09499d2cb56e06d2f45f497ff341e5f838de9
ZSabakh <zsabakhtarishvili@atu.edu>
2021-06-28 17:32:17 +0400

    Improved UI and made it responsive onClick

--- app/src/main/java/com/example/sosapp/ContactsRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/sosapp/ContactsRecyclerViewAdapter.kt
@@ -1,29 +1,45 @@
 package com.example.sosapp
 
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.annotation.ColorInt
+import androidx.annotation.RequiresApi
+import androidx.cardview.widget.CardView
+import androidx.core.graphics.toColor
+import androidx.core.graphics.toColorInt
 import androidx.recyclerview.widget.RecyclerView
 import com.example.sosapp.ui.models.ContactUIModel
 
 
-class ContactsRecyclerViewAdapter(private val contactUIModels: List<ContactUIModel>)
-    : RecyclerView.Adapter<ContactsRecyclerViewAdapter.ViewHolder>() {
+class ContactsRecyclerViewAdapter(private val contactUIModels: List<ContactUIModel>) :
+    RecyclerView.Adapter<ContactsRecyclerViewAdapter.ViewHolder>() {
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view: View = LayoutInflater.from(parent.context)
-            .inflate(R.layout.contact_element,parent,false)
+            .inflate(R.layout.contact_element, parent, false)
         return ViewHolder(view)
     }
 
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        var isSelected = false;
         holder.contact.text = contactUIModels[position].contactName
         holder.number.text = contactUIModels[position].phone
-        holder.itemView.setOnClickListener{
+        holder.itemView.setOnClickListener {
+            isSelected = !isSelected
             contactUIModels[position].onClick()
+            if (isSelected) {
+                holder.cvContactElement.setCardBackgroundColor(Color.parseColor("#4A653B"))
+            } else {
+                holder.cvContactElement.setCardBackgroundColor(Color.parseColor("#78A65F"))
+            }
         }
     }
 
@@ -33,9 +49,10 @@ class ContactsRecyclerViewAdapter(private val contactUIModels: List<ContactUIMod
     }
 
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val contact: TextView = itemView.findViewById(R.id.tv_contact)
         val number: TextView = itemView.findViewById(R.id.tv_number)
+        val cvContactElement: CardView = itemView.findViewById(R.id.cv_contact_element)
     }
 
 
