a8c29eb5086ee7b0230a5b3eec7e12a803d94266
ZSabakh <zsabakhtarishvili@atu.edu>
2021-07-08 17:44:00 +0400

    Can search through phone contacts when adding

--- app/src/main/java/com/example/sosapp/ui/ExistingContactsActivity.kt
+++ app/src/main/java/com/example/sosapp/ui/ExistingContactsActivity.kt
@@ -2,9 +2,15 @@ package com.example.sosapp.ui
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.sosapp.ContactsRecyclerViewAdapter
@@ -13,40 +19,67 @@ import com.example.sosapp.ui.models.ContactUIModel
 import com.example.sosapp.utility.ContactData
 import com.example.sosapp.utility.retrieveAllContacts
 
-class ExistingContactsActivity: AppCompatActivity() {
-    private lateinit var rvExistingContacts: RecyclerView
 
-    @SuppressLint("MissingPermission")
+class ExistingContactsActivity : AppCompatActivity() {
+    private lateinit var rvExistingContacts: RecyclerView
+    private lateinit var cvAddExisting: CardView
+    private lateinit var btAddExisting: Button
+    private lateinit var tvSelectedContactsCounter: TextView
+    private lateinit var etSearchExistingContacts: EditText
+    private val selectedContact: MutableList<ContactData> = ArrayList()
+    private val selectedContactIDs: MutableList<String> = ArrayList()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_existing_contacts)
         viewInitializations()
 
         rvExistingContacts.layoutManager = GridLayoutManager(this, 1)
-        var contacts: List<ContactData> = retrieveAllContacts()
-        contacts = contacts.filter { contact -> contact.phoneNumber.any()}
+        renderContacts(null)
 
+        etSearchExistingContacts.addTextChangedListener(object : TextWatcher {
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                renderContacts(etSearchExistingContacts.text.toString())
+            }
+
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun afterTextChanged(s: Editable) {
+
+            }
+        })
+
+    }
+
+    private fun renderContacts(contactName: String?) {
+        @SuppressLint("MissingPermission")
+        var contacts: List<ContactData> =  retrieveAllContacts()
+        contacts = contacts.filter { contact -> contact.phoneNumber.any() }
+        if(!contactName.isNullOrBlank()){
+            contacts = contacts.filter {contact -> contact.name.contains(contactName, ignoreCase = true)}
+        }
+
+        //Filter search with the above line too.
         rvExistingContacts.adapter = ContactsRecyclerViewAdapter(contacts.map {
-            println(it.phoneNumber[0])
             ContactUIModel(
                 it.name,
                 it.phoneNumber[0],
                 it.contactId.toString(),
                 onClick = {
-//                    btRemoveContacts.visibility = View.VISIBLE
-//                    btSendText.visibility = View.VISIBLE
-//                    if(!selectedContactNumbers.contains(it.phone)){
-//                        selectedContactNumbers.add(it.phone)
-//                        selectedContactIDs.add(it._id)
-//                    }else{
-//                        selectedContactNumbers.remove(it.phone)
-//                        selectedContactIDs.remove(it._id)
-//                    }
-//                    btSendText.text = "Send to ${selectedContactNumbers.size}"
-//                    if(selectedContactNumbers.size == 0){
-//                        btRemoveContacts.visibility = View.INVISIBLE
-//                        btSendText.visibility = View.INVISIBLE
-//                    }
+                    cvAddExisting.visibility = View.VISIBLE
+                    if (!selectedContactIDs.contains(it.contactId.toString())) {
+                        selectedContact.add(it)
+                        selectedContactIDs.add(it.contactId.toString())
+                    } else {
+                        selectedContact.remove(it)
+                        selectedContactIDs.remove(it.contactId.toString())
+                    }
+                    tvSelectedContactsCounter.text = "Save ${selectedContactIDs.size} contacts"
+                    if (selectedContactIDs.size == 0) {
+                        cvAddExisting.visibility = View.INVISIBLE
+                    }
                 },
             )
         })
@@ -54,5 +87,9 @@ class ExistingContactsActivity: AppCompatActivity() {
 
     fun viewInitializations() {
         rvExistingContacts = findViewById(R.id.rv_existing_contacts)
+        cvAddExisting = findViewById(R.id.cv_add_existing)
+        btAddExisting = findViewById(R.id.bt_add_existing)
+        etSearchExistingContacts = findViewById(R.id.et_search_existing_contacts)
+        tvSelectedContactsCounter = findViewById(R.id.tv_selected_contacts_counter)
     }
 }
